a64bcacb0b831af83034b2d43088aef619524101
Ống Tre <tamdaulong207@gmail.com>
2018-11-28 20:38:18 +0700

    -client design -login activity

--- HuChat/app/src/main/java/com/vnbamboo/huchat/LoginActivity.java
+++ HuChat/app/src/main/java/com/vnbamboo/huchat/LoginActivity.java
@@ -1,13 +1,53 @@
 package com.vnbamboo.huchat;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                startMainActivity();
+            }
+        });
+
+    }
+
+    public void startMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Bấm BACK lần nữa để thoát! ", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
