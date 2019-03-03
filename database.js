e593f8e474abd683ccd25f6d5d3c52180a88f100
tho.vo <votantho93@gmail.com>
2019-03-03 20:17:12 +0700

    split backend

--- database.js
+++
@@ -1,9 +0,0 @@
-var mysql = require("mysql");
-var connection = mysql.createPool({ 
-    host : "localhost",
-    user : "root",
-    password : "coffee",
-    database : "Huchat"
-});
-
-module.exports = connection;
